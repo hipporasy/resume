d67b4f9b22684b85667f402b99cc671c15d96b90
Hipporasy <hipporasy@gmail.com>
2022-01-08 22:24:20 +0700

    FIXED

--- src/utils/prerender.js
+++ src/utils/prerender.js
@@ -2,8 +2,8 @@
  * Returns true if being prerendered by react-snap. Useful for stuff
  * that needs to only run client-side and not during prerendering
  */
-let prerender;
-if (window && window.browser) {
-  prerender = navigator.userAgent === 'ReactSnap';
-}
+let prerender = false;
+// if (window && window.browser) {
+//   prerender = navigator.userAgent === 'ReactSnap';
+// }
 export default prerender;
