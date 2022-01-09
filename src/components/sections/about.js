273c4b9fa2084bb8b741cce805a53b2dae737f3a
hipporasy <hipporasy@gmail.com>
2022-01-09 14:53:15 +0700

    [UPDATED] - Intro

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,16 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'Swift',
+    'Kotlin',
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React.js',
+    'Next.js',
+    'Node.js',
+    'Nest.js',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +145,28 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hell! My name is Marasy and II like to learn new things, I love solving problems.
+              Throughout my career, I have been driven by my intellectual curiosity to provide solutions to the most
+              pressing problems.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://www.mango-byte.com">a start-up solution</a>,{' '}
+              <a href="https://hari.wtf/">a loyalty expertise</a>,{' '}
+              <a href="https://hanso.group">a consultancy</a>, and{' '}
+              <a href="https://wonderpass.asia">a new norm for COVID situation</a>. And I love supporting stray animal
+              and
+              trying to
+              build a community project for stray animal <a href="https://pettoapp.com/">PettoApp</a> for supporting
+              adoptions.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Last year, I made {' '}
+              <a href="https://www.youtube.com/channel/UCT1kUMoQLbU5jhUfTBl5jPA">
+                make a youtube channel
+              </a>{' '} for speed coding with SwiftUI and Flutter.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,30 +59,22 @@ const Hero = () => {
     const timeout = setTimeout(() => setIsMounted(true), navDelay);
     return () => clearTimeout(timeout);
   }, []);
-
   const one = <h1>Hi, my name is</h1>;
-  const two =  <DecoderText className="big-heading" text="Marasy Phi" start={!prerender} delay={300} />
-  const three = <h3 className="big-heading">I love developing.</h3>;
+  const two = <DecoderText className="big-heading" text="Marasy Phi" start={!prerender} delay={300}/>;
+  const three = <h3 className="big-heading">I love building amazing.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Get in Touch
-        </a>
-        .
+        I am a software engineer, I love starting up, love being involve in Entrepreneurship
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Get in touch
     </a>
   );
 
