4a4540e6849dc87ed9d166c7ca5b0aa257d50057
hipporasy <hipporasy@gmail.com>
2022-01-17 15:56:44 +0700

    FIXED

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Marasy Phi is a software engineer.',
     siteUrl: 'https://hipporasy.tech', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/logo.svg', // Path to your image you placed in the 'static' folder
     twitterUsername: '@hipporasy',
   },
   plugins: [
