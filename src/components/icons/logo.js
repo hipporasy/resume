930e0fc0becfdca0ec659997787c5aa2f6037742
Marasy Phi <hipporasy@gmail.com>
2022-06-18 15:40:32 +0700

    update resume

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -3,23 +3,17 @@ import React from 'react';
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(30.00000, 20.000000)">
-        <path fill="currentColor"
-              d="M 38.076 0 L 41.04 52.364 L 34.656 48 L 33.06 25.004 A 186.52 186.52 0 0 1 32.82 20.054 Q 32.604 14.127 32.604 6.08 L 23.788 42.94 L 17.176 42.94 L 7.752 6.08 Q 7.904 8.816 7.904 13.376 Q 7.904 20.199 7.615 24.779 A 96.081 96.081 0 0 1 7.6 25.004 L 6.232 48 L 0 48 L 2.964 0 L 12.16 0 L 20.672 35.872 L 28.804 0 L 38.076 0 Z"
+    <g>
+      <path fill="#171717" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
+            d="M 50, 5 L 11, 27 L 11, 72 L 50, 95 L 89, 73 L 89, 28 z" stroke-dasharray="270.1082763671875"
+            style="stroke-dashoffset: 0px;"></path>
+      <g id="M" transform="translate(29.000000, 21.000000)">
+        <path fill="#34d399"
+              d="M 38.076 0 L 41.04 52.364 L 34.656 52.364 L 33.06 25.004 A 186.52 186.52 0 0 1 32.82 20.054 Q 32.604 14.127 32.604 6.08 L 23.788 42.94 L 17.176 42.94 L 7.752 6.08 Q 7.904 8.816 7.904 13.376 Q 7.904 20.199 7.615 24.779 A 96.081 96.081 0 0 1 7.6 25.004 L 6.232 52.364 L 0 52.364 L 2.964 0 L 12.16 0 L 20.672 35.872 L 28.804 0 L 38.076 0 Z"
               vector-effect="non-scaling-stroke"/>
       </g>
-      <g transform="translate(11.000000, 5.000000)">
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
-      </g>
     </g>
+
   </svg>
 );
 
